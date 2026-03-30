8362ab4f63d30d46c29e4b7fe67c097184f5b75a
shailesh yadav <shailesh.3117y@gmail.com>
2026-03-30 19:06:35 +0530

    Update app.run to use environment variable for port

--- app.py
+++ app.py
@@ -304,6 +304,8 @@ def export():
 
     return send_file(file_path, as_attachment=True)
 
+import os
+
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
